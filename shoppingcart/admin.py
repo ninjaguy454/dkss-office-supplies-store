ed9689aa3a73b688bd9774698e36f305f09fa5ca
sjmarshall07 <70245767+sjmarshall07@users.noreply.github.com>
2021-06-04 21:57:55 -0500

    Added Customer Model
    Added customer model and added the "Customers" maintenance to the admin page.

--- shoppingcart/admin.py
+++ shoppingcart/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Customer
+
+class CustomerList(admin.ModelAdmin):
+    list_display = ( 'first_name', 'last_name', 'email', 'home_phone', 'work_phone', 'cell_phone', 'addr_line1','addr_line2', 'addr_line3', 'addr_line4', 'city', 'state', 'postal_code')
+    list_filter = ( 'cust_name', 'organization')
+    search_fields = ('cust_name', )
+    ordering = ['cust_name']
+
+
+admin.site.register(Customer)
 
 # Register your models here.
--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    home_phone = models.CharField(max_length=12, blank=True)
+    work_phone = models.CharField(max_length=12, blank=True)
+    cell_phone = models.CharField(max_length=12, blank=True)
+    addr_line1 = models.CharField(max_length=200)
+    addr_line2 = models.CharField(max_length=200, blank=True)
+    addr_line3 = models.CharField(max_length=200, blank=True)
+    addr_line4 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=50)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.first_name + ', ' + self.last_name)
