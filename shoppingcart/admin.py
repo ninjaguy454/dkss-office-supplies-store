03db35ac9e448f8a4cbe8048b7767cb24720f7f1
ninjaguy454 <dglenns3@ymail.com>
2021-06-05 04:15:24 -0500

    Added Admin Add/Edit/Remove Product Support
    Updated models.py
    - Added Product model
    
    Added Forms.py
    - Added Product model to form
    
    Udpated Admin.py
    - Added ProductList Class
    - Registered product to ProductList

--- shoppingcart/admin.py
+++ shoppingcart/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+# Model imports
+from .models import product
+
+#Admin Classes
+class ProductList(admin.ModelAdmin):
+    list_display = ('product_name', 'product_description', 'price')
+    list_filter = ('product_name', 'price')
+    search_fields = ['product_name']
+    ordering = ['product_name']
+
+
+# Model Registers
+admin.site.register(product, ProductList)
--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -1,3 +1,22 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+# Product model
+class product(models.Model):
+    product_name = models.CharField(max_length=100, help_text='Enter a product name (e.g. Stapler)')
+    product_description = models.TextField()
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return str(self.product_name)
